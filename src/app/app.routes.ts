75f8aeaa62f8e0db0e6fed9d739c165ecbe31285
Michał Zalewski <michal.zalewski1995@gmail.com>
2024-06-09 12:46:01 +0200

    Add ConverterComponent
    Add navigatation buttons

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
 import { tablesResolver } from './shared/tables/tables.resolver';
+import { ConverterComponent } from './converter/converter.component';
 
 export const routes: Routes = [
+    {
+        path: 'converter',
+        component: ConverterComponent,
+    },
     {
         path: '',
         component: MainPageComponent,
